54cdd4fdb8b644cc044303c1e2fcde055872d467
chaumdh <chauhuynh.0997@gmail.com>
2026-08-04 23:24:51 -0700

    add contact page

---
+++ Project 1/js/contact.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.page-heading').append('<h1>Contact Us</h1>');
+    $('main').append(`<p>We'd love to help. Send us an email using one of the buttons below and
+a Splashes representative will get back to you as soon as possible.`);
+});
