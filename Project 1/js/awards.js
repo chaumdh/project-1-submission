b07481a29650a2950f91736105b3a74c438aa39d
mramirez1upgrade <mramirez1@upgrade.com>
2026-08-11 20:30:27 -0700

    Addign awards.js

---
+++ Project 1/js/awards.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $('#awards').append(`
+        <div class="awards-container">
+            <h1>Awards We've Won</h1>
+
+            <div class="awards-grid">
+                <img src="images/awards/colorado-parenting-family-fav2021.png" alt="Colorado Parenting Family Favorite 2021">
+
+                <img src="images/awards/bay-area-parent-gold2020.png" alt="Bay Area Parent Family Favorite 2020">
+
+                <img src="images/awards/mla-swim-lessons-winner-2020-250x250.png" alt="Most Loved Swim Lessons 2020">
+
+                <img src="images/awards/colorado-parenting-family-fav2020.png" alt="Colorado Parenting Family Favorite 2020">
+
+                <img src="images/awards/website-parenting-oc-2019.jpeg" alt="Parenting OC Readers Choice 2019">
+
+                <img src="images/awards/colorado-parenting-family-fav2019.jpeg" alt="Colorado Parenting Family Favorite 2019">
+
+                <img src="images/awards/bay-area-parent-2019.jpeg" alt="Bay Area Parent Family Favorite 2019">
+
+                <img src="images/awards/best-of-la-kids-winner.jpeg" alt="Best of LA Kids Winner">
+
+                <img src="images/awards/gold-daisy-award-finalist-2018.jpeg" alt="Gold Daisy Awards Finalist 2018">
+
+                <img src="images/awards/best-of-the-best-2018.jpeg" alt="Best of the Best 2018">
+            </div>
+        </div>
+    `);
+
+});
