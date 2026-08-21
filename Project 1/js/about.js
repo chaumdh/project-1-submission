d4e4b7b1e5529a918b429d4c8fa2d9a2aa2cfec5
mramirez1upgrade <mramirez1@upgrade.com>
2026-08-20 21:02:31 -0700

    Ading about page

---
+++ Project 1/js/about.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    // About Hero
+    $('#about-content').append(`
+        <section id="about-hero">
+            <div class="hero-title">
+                <h1>About Splashes</h1>
+            </div>
+        </section>
+    `);
+
+
+    // Our Philosophy
+    $('#about-content').append(`
+        <section id="philosophy">
+            <div class="about-text">
+                <h2>Our Philosophy</h2>
+
+                <p>
+                    Our mission is to educate and motivate individuals to be safer
+                    and learn proper swimming technique.
+                </p>
+
+                <p>
+                    We have chosen to share our love of swimming with children and
+                    believe there is a swimmer in every child. We pride ourselves on
+                    teaching the necessary skills needed to become a comfortable,
+                    proficient swimmer right from the beginning.
+                </p>
+            </div>
+        </section>
+    `);
+
+
+    // We Do A Pretty Good Job
+    $('#about-content').append(`
+        <section id="good-job">
+            <div class="about-text">
+                <h2>We Do A Pretty Good Job</h2>
+
+                <p>
+                    Experience excellence through Splashes' award-winning programs
+                    where safety, fast progress, and a fun environment come first.
+                    Join the swim school that loves what they do and it shows.
+                </p>
+
+                <a href="#">
+                    Hear What Families Are Saying About Us
+                </a>
+            </div>
+        </section>
+    `);
+
+});
