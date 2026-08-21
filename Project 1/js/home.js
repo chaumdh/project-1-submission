d2b38e00bfcef563ca78e67e8cc9442b9f0bffae
chaumdh <chauhuynh.0997@gmail.com>
2026-08-20 23:40:37 -0700

    add find school locations for home page and fix some fonts

--- Project 1/js/home.js
+++ Project 1/js/home.js
@@ -6,10 +6,39 @@ $(document).ready(function() {
     $('.page-heading-buttons').addClass('d-flex justify-content-center align-items-center');
     $('.page-heading-buttons').append('<button class="btn btn-outline-primary rounded-pill bg-white p-4">Informational Video</button>');
     $('.page-heading-buttons').append('<button class="btn btn-outline-primary rounded-pill bg-white p-4">Swimvenger Video</button>');
+
+    // Define the HTML element you want to add
+    const newElement = '<div id="home-locations-map" class="d-grid justify-content-end col-6"></div>';
+    const breakpoint = 1200;
+
+    function handleScreenSize() {
+        const windowWidth = $(window).width();
+
+        // Check if the element already exists in the DOM
+        const elementExists = $('#home-locations-map').length > 0;
+
+        if (windowWidth >= breakpoint) {
+            // Add the element if it does not already exist
+            if (!elementExists) {
+                $('#home-locations').append(newElement);
+            }
+        } else {
+            // Remove the element if it exists on smaller screens
+            if (elementExists) {
+                $('#home-locations-map').remove();
+            }
+        }
+    }
+
+    // Run once on page load
+    handleScreenSize();
+
+    // Run every time the window is resized
+    $(window).on('resize', handleScreenSize);
 });
 
 // TODO 
-// reduce the logo size on smaller 
+// reduce the logo size on smaller screens
 // fix 2 buttons on heading for differnt screen sizes
-// add styling for hovering over button in header
 // find a location near you, and at diff screen sizes
+// header nav + button: add styling for hover
